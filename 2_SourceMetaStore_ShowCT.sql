56d4f054af3aac80dcb0bc216fb323abbf554f08
Anish Chakraborty <anish749@users.noreply.github.com>
2016-03-28 12:25:09 +0530

    Update 2_SourceMetaStore_ShowCT.sql

--- 2_SourceMetaStore_ShowCT.sql
+++ 2_SourceMetaStore_ShowCT.sql
@@ -1,5 +1,7 @@
 -- Query for Creating Show CT Statements to be run in the Source Hive Installation
 
+set @SCHEMA_TO_MIGRATE = '%ani%';
+
 SELECT 
     CONCAT('SHOW CREATE TABLE ',
             D.name,
@@ -12,5 +14,5 @@ FROM
     DBS D ON D.DB_ID = T.DB_ID
 WHERE
     T.TBL_TYPE != 'INDEX_TABLE'
-        AND D.name LIKE @SCHEMA_TO_MIGRATE INTO OUTFILE 'generatedSQL/target/2_CT.hql';
+        AND D.name LIKE @SCHEMA_TO_MIGRATE INTO OUTFILE 'generatedSQL_source/2_CT.hql';
 		
