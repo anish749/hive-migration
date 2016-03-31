78e8b740837555a71ceea78987e5e70b7f6ef14e
Anish Chakraborty <anish749@users.noreply.github.com>
2016-03-31 11:37:38 +0530

    Update 3_SourceMetaStore_AddPartition.sql
    Made query simpler, removed usage of lag hack to get previous DB name to remove use query in case of same DB name.

--- 3_SourceMetaStore_AddPartition.sql
+++ 3_SourceMetaStore_AddPartition.sql
@@ -1,27 +1,18 @@
 -- Query to create add partition scripts to run in target Hive Installation
 
 SET @SCHEMA_TO_MIGRATE = '%ani%';
-SET @prevDB = 'nodbxyzabc_0b040aca_d613_4c47_9e2c_b21daafba13f'; -- Note - this DB should not exist in the source Hive Installation
 SET @oldNameNode = 'hdfs://localhost:9000/'; -- Old NameNode address
 SET @newNameNode = 'hdfs://newhostName:8020/'; -- New NameNode address
 
 SELECT 
-    REPLACE(add_partitions_script,
+    REPLACE(add_partition_query,
         @oldNameNode,
-        @newNameNode) AS add_partitions_script
-FROM
-    (SELECT 
-        @prevDB prev,
-            @prevDB:=D.Name curr,
-            CASE
-                WHEN
-                    @prevDB <> D.Name
-                THEN
-                    CONCAT('USE ', D.name, '\; ', ' ALTER TABLE ', T.TBL_NAME, ' ADD PARTITION (', GROUP_CONCAT(PK.PKEY_NAME, '=', '\'', PKV.PART_KEY_VAL, '\''
-                        ORDER BY PK.INTEGER_IDX), ') LOCATION \'', S.location, '\'; ')
-                ELSE CONCAT(' ALTER TABLE ', T.TBL_NAME, ' ADD PARTITION (', GROUP_CONCAT(PK.PKEY_NAME, '=', '\'', PKV.PART_KEY_VAL, '\''
-                    ORDER BY PK.INTEGER_IDX), ') LOCATION \'', S.location, '\'\; ')
-            END AS add_partitions_script
+        @newNameNode) AS add_partition_query
+	
+FROM			
+	(SELECT
+		CONCAT('USE ', D.name, '\; ', ' ALTER TABLE ', T.TBL_NAME, ' ADD PARTITION (', GROUP_CONCAT(PK.PKEY_NAME, '=', '\'', PKV.PART_KEY_VAL, '\''
+                    ORDER BY PK.INTEGER_IDX), ') LOCATION \'', S.location, '\'\; ') AS add_partition_query
     FROM
         TBLS T
     INNER JOIN DBS D ON T.DB_ID = D.DB_ID
@@ -33,7 +24,6 @@ FROM
     WHERE
         D.name LIKE @SCHEMA_TO_MIGRATE
     GROUP BY P.PART_ID
-    ORDER BY D.name) alias1 INTO OUTFILE 'generatedSQL_target/3_ADD_PART.hql'	
-	
- 
- 
+    ORDER BY D.name) alias1 INTO OUTFILE 'generatedSQL_target/3_ADD_PARTITION.hql';
+    
+    
